cfe84611570be5c497d9492ea49746e246685202
Harsh dholakiya <harshdholakiya2111@gmail.com>
2026-02-27 17:58:04 +0530

    UI: Adjust responsive breakpoints on Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     useEffect(() => {
         const handleResize = () => {
             // Apply responsive design below 1475x773
-            const isSmallScreen = window.innerWidth < 1460 || window.innerHeight < 700
+            const isSmallScreen = window.innerWidth < 1400 || window.innerHeight < 650
             setIsResponsive(isSmallScreen)
         }
 
@@ -26,23 +26,23 @@ const Home = () => {
                 <div className='home relative bg-gray-200 overflow-hidden min-h-screen flex flex-col items-center justify-center'>
                     <div className='text relative pt-8 px-6 text-center'>
                         <TypewriterHeader />
-                        
+
                         {/* Social Links - Horizontal Row */}
                         <div className='flex flex-row gap-6 items-center justify-center pt-8'>
                             <button className='hover:scale-110 transition-transform' title='Github'>
                                 <a href="https://github.com/harshdholakiya21" target='_blank'>
-                                    <img src="image/github.svg" alt="github" className='w-12 rounded-2xl overflow-hidden'/> 
-                                </a> 
+                                    <img src="image/github.svg" alt="github" className='w-12 rounded-2xl overflow-hidden' />
+                                </a>
                             </button>
                             <button className='hover:scale-110 transition-transform' title='LinkedIn'>
-                                <a href="https://www.linkedin.com/in/harshdholakiya/" target='_blank'> 
-                                    <img src="image/linkedln.svg" alt="linkedin" className='w-12'/> 
-                                </a> 
+                                <a href="https://www.linkedin.com/in/harshdholakiya/" target='_blank'>
+                                    <img src="image/linkedln.svg" alt="linkedin" className='w-12' />
+                                </a>
                             </button>
                             <button className='hover:scale-110 transition-transform' title='Contact'>
-                                <NavLink to={"/contact"}> 
-                                    <img src="image/contact.svg" alt="contact" className='w-12'/> 
-                                </NavLink> 
+                                <NavLink to={"/contact"}>
+                                    <img src="image/contact.svg" alt="contact" className='w-12' />
+                                </NavLink>
                             </button>
                         </div>
                     </div>
@@ -52,22 +52,22 @@ const Home = () => {
                 <div className='home relative bg-gray-200 overflow-hidden max-h-screen'>
                     <div className='text absolute top-1/5'>
                         <TypewriterHeader />
-                        
+
                         <div className='flex gap-20 pt-20 pl-20 items-center'>
                             <button className='hover:scale-110' title='Github'>
                                 <a href="https://github.com/harshdholakiya21" target='_blank'>
-                                    <img src="image/github.svg" alt="github" className='rounded-2xl overflow-hidden'/> 
-                                </a> 
+                                    <img src="image/github.svg" alt="github" className='rounded-2xl overflow-hidden' />
+                                </a>
                             </button>
                             <button className='hover:scale-110' title='LinkedIn'>
-                                <a href="https://www.linkedin.com/in/harshdholakiya/" target='_blank'> 
-                                    <img src="image/linkedln.svg" alt="linkedin" /> 
-                                </a> 
+                                <a href="https://www.linkedin.com/in/harshdholakiya/" target='_blank'>
+                                    <img src="image/linkedln.svg" alt="linkedin" />
+                                </a>
                             </button>
                             <button className='hover:scale-110' title='Contact'>
-                                <NavLink to={"/contact"}> 
-                                    <img src="image/contact.svg" alt="contact" /> 
-                                </NavLink> 
+                                <NavLink to={"/contact"}>
+                                    <img src="image/contact.svg" alt="contact" />
+                                </NavLink>
                             </button>
                         </div>
                     </div>
